08800ce729e3b1425c65b2d61efb19ac9cd61ea4
Mark Johnson <markjohnson4542@gmail.com>
2018-02-05 14:15:29 -0500

    GET /cats

--- api.js
+++ api.js
@@ -6,7 +6,25 @@ const {propOr} = require('ramda')
 const bodyParser = require('body-parser')
 const port = propOr(5000,'PORT',process.env) // process.env.PORT || 5000
 
+
+const cats = [
+ {name: "peanut",
+  age: 2,
+  gender: "male"}
+  ,{name: 'frisky', age: 13, gender: 'female'},{name: 'mac', age: 12, gender: 'male'},
+  {name: 'cheese', age: 11, gender: 'female'},
+]
 app.get('/', ( req, res ) =>   res.send('meow!')     )
+
+
+
+
+
+
+app.get('/cats', (req, res) => res.send(cats))
+
+
+
 
 
 app.listen(port, () => console.log('listening on port', port ))
